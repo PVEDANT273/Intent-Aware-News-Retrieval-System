31ebb59fea6712cc4fed98f3d6821bd58a8211b9
Vedant Patil <pvedant090@gmail.com>
2026-01-23 12:08:52 +0530

    Made some changes in the output of rank function and added a method

--- reasoner.py
+++ reasoner.py
@@ -1,4 +1,6 @@
 from ollama import Client
+import re
+import json
 
 class Reasoner:
     def __init__(self, list_of_groups, topic, intent):
@@ -54,5 +56,33 @@ class Reasoner:
             options={"temperature": 0.3}
         )
 
-        return response["message"]["content"]
+        raw = response["message"]["content"].strip()
+
+        raw = re.sub(r"```json|```", "", raw).strip()
+
+        start = raw.find("[")
+        end = raw.rfind("]") + 1
+
+        if start == -1 or end == -1:
+            raise ValueError("No JSON found in LLM response")
+
+        json_str = raw[start:end]
+
+        return json.loads(json_str)
+    
+    
+    def get_original_articles(self, llm_json : list, original_grouped : list):
+        results = []
+
+        for item in llm_json:
+            group_id = item["group_id"]
+
+        results.append({
+            "group_id": group_id,
+            "rank": item["rank"],
+            "reason": item["reason"],
+            "articles": original_grouped[group_id]
+        })
+
+        return results
 
